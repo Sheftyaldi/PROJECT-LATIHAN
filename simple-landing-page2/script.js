4e71c2e3ee32358f85c8a3884ff1083d0fd43807
Sheftyaldi <130757857+Sheftyaldi@users.noreply.github.com>
2024-08-24 23:59:52 +0700

    Update script.js

--- simple-landing-page2/script.js
+++ simple-landing-page2/script.js
@@ -1,6 +1,7 @@
 // document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('hamburger');
     const menu = document.getElementById('tbl');
+let non;
 
     hamburger.addEventListener('click', function(){
         menu.classList.toggle('active')
